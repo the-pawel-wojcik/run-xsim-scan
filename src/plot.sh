895e26a6ca31d106f2f7e7052a10853fc4a50f90
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-03-24 16:29:40 -0400

    Split plot command

--- src/plot.sh
+++ src/plot.sh
@@ -14,5 +14,12 @@ do
     array=(${base//_/ })
     case_name=${array[1]}
     echo Plotting $case_name
-    xsimplot.py -f "$case_name".svg --spectrum_format 'xsim' --spectrum_files ${base}/${base}.output --annotate "a${case_name}" --scale_factor 0.7 --position_annotation 'top center' --second_ax 'cm offset'
+    xsimplot.py\
+        -f "$case_name".svg\
+        --spectrum_format 'fort.20'\
+        --spectrum_files A-${base}/fort.2 B-${base}/fort.200 C-${base}/fort.20\
+        --annotate "a${case_name}" #\
+        # --scale_factor 0.7\
+        # --position_annotation 'top center'\
+        # --second_ax 'cm offset'
 done
